c090fa960812b9ab2b5a4b6ed39e8951b9341e8b
d1azdn <diazsaputramc@gmail.com>
2025-04-18 09:37:40 +0700

    added login-register ratelimiter

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,57 +1,14 @@
 const express = require("express")
 require('dotenv').config();
-const { google } = require('googleapis')
+const db = require("../db")
 const jwt = require('jsonwebtoken')
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URL
-)
-
-const scopes = [
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile"
-]
-
-const authUrl = oauth2Client.generateAuthUrl({
-    acces_type: 'offline',
-    scope: scopes,
-})
-
-const authGoogle = async (req,res) => {
-    console.log("masok")
-    res.redirect(authUrl)
+const authLogin = (req,res,) => {
+    res.send('login berhasil')
 }
 
-const authGoogleCallback = async (req,res) => {
-    const code = req.query.code
-    console.log(code)
+const authRegister = (req,res) => {
+    res.send('register berhasil')
+} 
 
-    const { tokens } = await oauth2Client.getToken(code)
-    oauth2Client.setCredentials(tokens)
-
-    const oauth2 = google.oauth2({
-        version: 'v2',
-        auth: oauth2Client
-    });
-
-    console.log("masokss")
-    const { data } = await oauth2.userinfo.get();
-
-    const token = jwt.sign(
-        {
-          id: data.id,
-          name: data.name,
-          email: data.email,
-          picture: data.picture
-        },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: '1h' }
-      );
-
-    res.json(token)
-}
-
-
-module.exports = { authGoogle, authGoogleCallback }
+module.exports = { authLogin, authRegister }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,31 @@ require('dotenv').config();
 
 const express = require("express")
 const router = express.Router();
-const { authGoogle, authGoogleCallback } = require("../controllers/authController")
+const { authGoogle, authGoogleCallback } = require("../controllers/authGoogleController")
+const { authLogin, authRegister } = require("../controllers/authController")
 
+const rateLimit = require('express-rate-limit');
 
-router.get('/google', authGoogle)
-router.get('/google/callback', authGoogleCallback)
+const loginLimit = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 5,
+    message: 'Terlalu banyak percobaan. Coba lagi dalam 5 menit.',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const registerLimit = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 5,
+    message: 'Terlalu banyak percobaan. Coba lagi dalam 10 menit.',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+router.get('/google', loginLimit, authGoogle)
+router.get('/google/callback', loginLimit, authGoogleCallback)
+router.post('/login', loginLimit, authLogin)
+router.post('/register', registerLimit, authRegister)
 
 
 module.exports = router;
